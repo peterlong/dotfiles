2030ef08cc3472c9fd54c863021a9e31e84a0882
Peter Long <peter.long@hs2solutions.com>
2016-03-01 22:12:19 -0500

    Try make git RPROMPT work

--- zsh/prompt.zsh
+++ zsh/prompt.zsh
@@ -67,4 +67,4 @@ precmd() {
 }
 
 export PROMPT='%(?.%F{205}.%F{red})❯%f '
-export RPROMPT="`git_dirty`%F{241}$vcs_info_msg_0_%f `git_arrows``suspended_jobs`"
+export RPROMPT='`git_dirty`%F{241}$vcs_info_msg_0_%f `git_arrows``suspended_jobs`'
